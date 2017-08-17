5d5a80652c77829a18a9c4f8fb80b93e7d0a82c9
Ryan Armstrong <rarmstrong@sofi.org>
2017-08-17 13:26:20 -0600

    renamed name tag to a more descriptive name.

--- us-east-1/route_table_public.tf
+++ us-east-1/route_table_public.tf
@@ -2,8 +2,8 @@ module "public_routetable" {
   source = "../modules/route_table"
 
   vpc_id     = "${module.vpc_sandbox.vpc_id}"
-  name       = "gateways"
-  gateway_id = "${module.internet_gateway.internet_gw_id}"
+  name       = "public_gateway"
+  internet_gateway_id = "${module.internet_gateway.internet_gw_id}"
 
   # XXX - https://serialseb.com/blog/2016/05/11/terraform-working-around-no-count-on-module/
   num_subnets = "1"
