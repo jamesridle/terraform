7d931687bb0d526699c2edbb9aa4c647b2d5552b
Ryan Armstrong <rarmstrong@sofi.org>
2017-08-17 13:25:34 -0600

    renamed name tag to a more decriptive name.

--- us-east-1/route_table_private.tf
+++ us-east-1/route_table_private.tf
@@ -2,7 +2,7 @@ module "private_routetable" {
   source = "../modules/route_table"
 
   vpc_id         = "${module.vpc_sandbox.vpc_id}"
-  name           = "nat"
+  name           = "nat_to_public_gateway"
   nat_gateway_id = "${module.nat_gateways.nat_gw_ids[0]}"
 
   # XXX - https://serialseb.com/blog/2016/05/11/terraform-working-around-no-count-on-module/
