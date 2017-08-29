8e81094bb93256e59d3340b1daa9b8c5c3671562
Ryan Armstrong <rarmstrong@sofi.org>
2017-08-29 09:42:39 -0600

    took out prod state data source and put it in the staging env.

---
+++ us-east-2/staging/aws_provider.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_availability_zones" "available" {}
+
+data "terraform_remote_state" "prod-east-2" {
+  backend = "s3"
+
+  config {
+    bucket = "terraform.rarmstrong-sandbox.com"
+    key    = "us-east-2/prod/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
